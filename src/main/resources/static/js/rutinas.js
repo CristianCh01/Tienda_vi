bfaf5a1911d78128c09dea728a6f1f5e96878c3b
crisc <crisc@Cristian>
2025-04-04 13:41:50 -0600

    Semana 12 Carrito parte 1 / 2

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,13 +1,26 @@
 //Esta funcion carga una imagen local en la pagina html
-function readURL(input){
-    if (input.files && input.files[0]){
+function readURL(input) {
+    if (input.files && input.files[0]) {
         //Nos pasaron un archivo de imagen
-        var lector = new FileReader ();
-        lector.onload = function(e) {
-            $('#blah').attr('src',e.target.result)
+        var lector = new FileReader();
+        lector.onload = function (e) {
+            $('#blah').attr('src', e.target.result)
                     .height(200);
         };
-        lector.readAsDataURL (input.files[0]);
+        lector.readAsDataURL(input.files[0]);
+    }
+}
+
+//Esta funcion carga en el carrito de compras un producto seleccionado por el usuario
+function addCart(formulario) {
+    var idProducto = formulario.elements[0].value;
+    var existencias = formulario.elements[1].value;
+    if (existencias > 0) {
+        //Se incluye el producto en el carrito
+        var ruta ="/carrito/agregar/" +idProducto;
+        $("#resultBlock").load(ruta);
+    } else {
+        window.alert("No hay existencias...");
     }
 }
 
