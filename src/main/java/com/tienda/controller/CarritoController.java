732e3fdeacc771dddb39a5a8ad358abfab56bc0b
crisc <crisc@Cristian>
2025-04-10 16:29:37 -0600

    Semana 13 Carrito parte 2 / 2

--- src/main/java/com/tienda/controller/CarritoController.java
+++ src/main/java/com/tienda/controller/CarritoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,36 +20,57 @@ public class CarritoController {
     private ItemService itemService;
     @Autowired
     private ProductoService productoService;
-    
+
     @GetMapping("/agregar/{idProducto}")
     public ModelAndView agregar(Model model, Item item) {
         Item item2 = itemService.getItem(item);
-        
+
         // Si no existe el item en el carrito, se busca la info del producto
-        if (item2==null) {
-            Producto p =productoService.getProducto(item);
+        if (item2 == null) {
+            Producto p = productoService.getProducto(item);
             item2 = new Item(p);
         }
-        
+
         //Finalmente se guarda en la variable de session el producto del carrito
         itemService.save(item2);
-        
+
         //Se crean elemento para mostrar en el el fragmento "verCarrito"
-        var lista =itemService.getItems();
-        var totalCompra=itemService.getTotal();
-        
+        var lista = itemService.getItems();
+        var totalCompra = itemService.getTotal();
+
         model.addAttribute("listaItems", lista);
         model.addAttribute("totalCompra", totalCompra);
         model.addAttribute("totalProductos", lista.size());
-        
+
         return new ModelAndView("/carrito/fragmentos :: verCarrito");
 
     }
-    
+
     @GetMapping("/listado")
     public String listado(Model model) {
-        var lista =itemService.getItems();
+        var lista = itemService.getItems();
+        var totalCompra = itemService.getTotal();
         model.addAttribute("listaItems", lista);
+        model.addAttribute("totalCompra", totalCompra);
         return "/carrito/listado";
+    }
+
+    @GetMapping("/eliminar/{idProducto}")
+    public String eliminar(Model model, Item item) {
+        itemService.delete(item);
+        return "redirect:/carrito/listado";
+    }
+
+    @GetMapping("/modificar/{idProducto}")
+    public String modificar(Model model, Item item) {
+        item = itemService.getItem(item);
+        model.addAttribute("item", item);
+        return "/carrito/modifica";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Item item) {
+        itemService.update(item);
+        return "redirect:/carrito/listado";
     }
 }
